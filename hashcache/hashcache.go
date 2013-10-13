a31c8056804e222592143eff78a77ff0037f4fdb
Dmitry Chestnykh <dmitry@codingrobots.com>
2013-10-13 14:33:29 +0200

    hashcash: simplify to use Gob encoding for files.

--- hashcache/hashcache.go
+++ hashcache/hashcache.go
@@ -2,12 +2,9 @@
 package hashcache
 
 import (
-	"bufio"
 	"crypto/md5"
-	"encoding/binary"
-	"fmt"
+	"encoding/gob"
 	"hash"
-	"io"
 	"os"
 	"sync"
 )
@@ -18,8 +15,6 @@ const (
 
 	MaxPathLen = 4096
 )
-
-var hashFunc = md5.New
 
 type Cache struct {
 	sync.Mutex
@@ -65,42 +60,7 @@ func (c *Cache) WriteToFile(filename string) (err error) {
 			os.Remove(filename)
 		}
 	}()
-	w := bufio.NewWriter(f)
-	// Write version.
-	err = w.WriteByte(fileFormatVersion)
-	if err != nil {
-		return
-	}
-	c.Lock()
-	defer c.Unlock()
-	// Write length of map.
-	err = binary.Write(w, binary.LittleEndian, uint64(len(c.m)))
-	if err != nil {
-		return
-	}
-	for path, hash := range c.m {
-		// Write length of path.
-		if len(path) > MaxPathLen {
-			err = fmt.Errorf("path length %d is too big", len(path))
-			return
-		}
-		err = binary.Write(w, binary.LittleEndian, uint16(len(path)))
-		if err != nil {
-			return
-		}
-		// Write path.
-		_, err = w.WriteString(path)
-		if err != nil {
-			return
-		}
-		// Write hash.
-		_, err = w.Write(hash[:])
-		if err != nil {
-			return
-		}
-	}
-	err = w.Flush()
-	return
+	return gob.NewEncoder(f).Encode(c.m)
 }
 
 func NewFromFile(filename string) (c *Cache, err error) {
@@ -109,55 +69,10 @@ func NewFromFile(filename string) (c *Cache, err error) {
 		return
 	}
 	defer f.Close()
-	r := bufio.NewReader(f)
-	// Read version.
-	ver, err := r.ReadByte()
+	m := make(map[string][hashSize]byte)
+	err = gob.NewDecoder(f).Decode(&m)
 	if err != nil {
 		return
-	}
-	if ver != fileFormatVersion {
-		err = fmt.Errorf("wrong file format version: %d", ver)
-		return
-	}
-	// Read length of map.
-	var mapLen uint64
-	err = binary.Read(r, binary.LittleEndian, &mapLen)
-	if err != nil {
-		return
-	}
-	// Limit this allocation hint to something reasonable.
-	if mapLen > 100000 {
-		mapLen = 100000
-	}
-	m := make(map[string][hashSize]byte, mapLen)
-	var hash [hashSize]byte
-	for {
-		// Read length of path.
-		var pathLen uint16
-		err = binary.Read(r, binary.LittleEndian, &pathLen)
-		if err != nil {
-			if err == io.EOF {
-				err = nil
-				break
-			}
-			return
-		}
-		if pathLen > MaxPathLen {
-			return nil, fmt.Errorf("path length %d is too big", pathLen)
-		}
-		// Read path.
-		pathBytes := make([]byte, int(pathLen))
-		_, err = r.Read(pathBytes[:])
-		if err != nil {
-			return
-		}
-		// Read hash.
-		_, err = r.Read(hash[:])
-		if err != nil {
-			return
-		}
-		// Set.
-		m[string(pathBytes)] = hash
 	}
 	return &Cache{m: m, h: md5.New()}, nil
 }
