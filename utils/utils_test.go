54e6a8269ed2df133034d97018a50251f4d43aa2
Dmitry Chestnykh <dmitry@codingrobots.com>
2014-02-07 01:52:49 +0100

    Forgot to commit utils_test.go.

---
+++ utils/utils_test.go
@@ -0,0 +1,22 @@
+package utils
+
+import "testing"
+
+func TestAbsPaths(t *testing.T) {
+	var tests = []struct{ in, out string }{
+		{
+			`Hello <img class="img-responsive" src="/img/hello.png" alt=""> world!`,
+			`Hello <img class="img-responsive" src="http://example.com/img/hello.png" alt=""> world!`,
+		},
+		{
+			`Click <a href="/go/to">this\nlink</a>! But not <a href="http://google.com/calendar">this</a>.`,
+			`Click <a href="http://example.com/go/to">this\nlink</a>! But not <a href="http://google.com/calendar">this</a>.`,
+		},
+	}
+	for i, v := range tests {
+		out := AbsPaths("http://example.com", v.in)
+		if v.out != out {
+			t.Errorf("%d: expected\n%s\ngot\n%s\n", i, v.out, out)
+		}
+	}
+}
