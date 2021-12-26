ce439aebab0654b03af1aebe89c3db7391e3b68b
Dmitry Chestnykh <dmitry@codingrobots.com>
2021-12-26 22:45:39 +0100

    Fix error logging in search script minification

--- search/search.go
+++ search/search.go
@@ -17,8 +17,9 @@ var mainScript string
 func GetSearchScript(searchIndexURL string) string {
 	out := stemmer + strings.ReplaceAll(mainScript, "__KKR_SEARCH_INDEX_URL__", searchIndexURL)
 	minified, err := jsmin.Minify([]byte(out))
-	if err == nil {
+	if err != nil {
 		log.Printf("Failed to minify search-script, continuing with unminified")
+	} else {
 		out = string(minified)
 	}
 	return out
